a37920ea02a81303c02775a5a742e7548ce8262a
Jason Triemert <jaytriemert@gmail.com>
2023-05-19 23:22:08 -0500

    added product routes and worked with tutor

--- main/routes/api/product-routes.js
+++ main/routes/api/product-routes.js
@@ -4,27 +4,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category},{ model: Tag, through: ProductTag}]
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category},{ model: Tag, through: ProductTag}]
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
+/* req.body should look like this...
+{
+  product_name: "Basketball",
+  price: 200.00,
+  stock: 3,
+  tagIds: [1, 2, 3, 4]
+}
+*/
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -89,7 +103,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destory({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      return res.status(400).json({ message: "No product found"})
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
